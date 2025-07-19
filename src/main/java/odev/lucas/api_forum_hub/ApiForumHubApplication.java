4cc49138089f277ae8340c60cd02fedf62d85e4f
calixtollucas <lucaslucenacalixto1@gmail.com>
2025-07-19 11:29:28 -0300

    initial commit

---
+++ src/main/java/odev/lucas/api_forum_hub/ApiForumHubApplication.java
@@ -0,0 +1,13 @@
+package odev.lucas.api_forum_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForumHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/odev/lucas/api_forum_hub/ApiForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package odev.lucas.api_forum_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
