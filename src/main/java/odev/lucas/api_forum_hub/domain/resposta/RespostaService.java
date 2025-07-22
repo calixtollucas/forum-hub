c39a5916436683c16fd378b404e85d2f8a0340b9
calixtollucas <lucaslucenacalixto1@gmail.com>
2025-07-22 11:39:11 -0300

    add service

---
+++ src/main/java/odev/lucas/api_forum_hub/domain/resposta/RespostaService.java
@@ -0,0 +1,11 @@
+package odev.lucas.api_forum_hub.domain.resposta;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RespostaService {
+
+    private final RespostaRepository respostaRepository;
+}
